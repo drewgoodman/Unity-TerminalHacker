85329df636c544ca18d52179fda832eaba59a2fd
rdogm13 <drg6525@yahoo.com>
2021-01-30 01:46:39 -0800

    basic gameplay loop w/ screen state

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -6,6 +6,10 @@ public class Hacker : MonoBehaviour
 {
     // GAME STATE
     int level;
+    string password;
+    string passwordHint;
+    enum Screen { MainMenu, Password, Win };
+    Screen currentScreen;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +19,7 @@ public class Hacker : MonoBehaviour
 
     void ShowMainMenu()
     {
+        currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
         Terminal.WriteLine("Welcome to Terminal Hacker.\n\nWhat would you like to hack today?\n");
         Terminal.WriteLine("1. Hack an Instagram account");
@@ -26,11 +31,23 @@ public class Hacker : MonoBehaviour
 
     void OnUserInput(string input)
     {
-        if (input == "menu")
+        if (input == "menu") // we can always go direct to main menu
         {
             ShowMainMenu();
         }
-        else if (input == "007")
+        else if (currentScreen == Screen.MainMenu)
+        {
+            RunMainMenu(input);
+        }
+        else if (currentScreen == Screen.Password)
+        {
+            RunPassword(input);
+        }
+    }
+
+    void RunMainMenu(string input)
+    {
+        if (input == "007")
         {
             Terminal.WriteLine("Please select a level, Mr. Bond.");
         }
@@ -47,7 +64,40 @@ public class Hacker : MonoBehaviour
 
     void StartGame()
     {
+        currentScreen = Screen.Password;
         Terminal.WriteLine("You have chosen level " + level);
+        if (level == 1)
+        {
+            Terminal.WriteLine("Log into your Instagram Account!");
+            password = "follow";
+            passwordHint = "WLOOLF";
+
+        }
+        else if (level == 2)
+        {
+            Terminal.WriteLine("Log into the DMV registry!");
+            password = "traffic";
+            passwordHint = "FARCFIT";
+        }
+        else if (level == 3)
+        {
+            Terminal.WriteLine("Log into your Swiss Bank Account!");
+            password = "dividend";
+            passwordHint = "IIDNEVDN";
+        }
+        Terminal.WriteLine("PASSWORD HINT: " + passwordHint);
+        Terminal.WriteLine("Please enter your password: ");
+    }
+
+    void RunPassword(string input)
+    {
+        if (input == password) {
+            Terminal.WriteLine("That's correct!");
+        }
+        else
+        {
+            Terminal.WriteLine("Incorrect, try again. HINT: " + passwordHint);
+        }
     }
 
 
